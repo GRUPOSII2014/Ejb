73cdccce6612e1af8c1f7f34bff89662dab07a97
fernando <fernando-sm93@uma.es>
2014-05-17 13:33:29 +0200

    Modificaciones sobre ingreso, tratamiento y crearcita

---
+++ IngresoEjb.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Ejb;
+
+import Entidades.Cama;
+import Entidades.Cita;
+import Entidades.Persona;
+import Entidades.Tratamiento;
+
+/**
+ *
+ * @author Fernando
+ */
+public interface IngresoEjb {
+    
+    public Persona getPersona(Integer nss);
+    public void asignarCama(Persona p, Cama c);
+    public Cama primeraLibre();
+    public void terminarTratamiento(Tratamiento t);
+    public void crearCita(Persona p, Cita c);
+}
