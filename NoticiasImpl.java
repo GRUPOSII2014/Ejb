38c91eea60a5098a12e84db0685ea0181dd3bc93
Emilio Tenorio <emiliotenorio@gmail.com>
2014-05-20 17:58:47 +0200

    EJB Noticias

--- NoticiasImpl.java
+++ NoticiasImpl.java
@@ -8,6 +8,7 @@ package Ejb;
 
 import Entidades.Noticia;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -24,9 +25,18 @@ public class NoticiasImpl implements NoticiasEJB {
     @PersistenceContext(unitName = "HospitalEE-ejbPU")
     private EntityManager em;
     
+    @EJB
+    private PersonaEjb persona;
+    
     @Override
     public List<Noticia> allNoticias() {
         TypedQuery<Noticia> query = em.createNamedQuery("Noticias", Noticia.class);
         return query.getResultList();
+    }
+    
+    @Override
+    public void setNoticia(Noticia n, Integer nss) {
+        n.setEscritoPor(persona.getTrabajador(nss));
+        em.persist(n);
     }
 }
